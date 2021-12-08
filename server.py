85079d6bb9cda7ced374be14ee95a92587559051
chethanreddy1 <95535293+chethanreddy1@users.noreply.github.com>
2021-12-08 17:45:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,11 +4,9 @@ import threading
 
 
 PORT = 5050
-HEADER= 64
 FORMAT = 'utf-8'
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-JJ = "cncncn"
 DISCONNECTMESSAGE= 'BYE!3808fuhdh32u'
 lcon= []
 laddr= []
@@ -17,7 +15,7 @@ server.bind(ADDR)
 
 def handle_client(conn, addr):
     connected = True
-    def sendclient(msg):
+    def sendclients(msg):
         for c in lcon:
              try:
                if not c==conn:
@@ -25,7 +23,7 @@ def handle_client(conn, addr):
              except:
                  pass
 
-    sendclient(f"{addr} has joined the chat")
+    sendclients(f"{addr} has joined the chat")
     print(f"{addr} has joined the chat")
     while connected:
      try:
@@ -33,23 +31,29 @@ def handle_client(conn, addr):
            
            if msg == DISCONNECTMESSAGE:
                connected = False
-               sendclient(f"{addr} has left the chat")
+               lcon.remove(conn)
+               laddr.remove(addr)
+               sendclients(f"{addr} has left the chat")
                print(f"{addr} has left the chat")
+               break
+
+           if msg.startswith("/private"):
+            
+               
+               paddr= (msg[9:21], int(msg[23:28]))
+              
+               index = laddr.index(paddr)
+               lcon[index].send(msg[29:].encode(FORMAT))
+               
            else:
-             if msg.startswith("/private"):
-              print(msg)
-              paddr= (msg[9:21], int(msg[23:28]))
-              print(paddr)
-              index = laddr.index(paddr)
-              lcon[index].send(msg[30:].encode(FORMAT))
-             else:
-               sendclient(f'{addr}: {msg}')
+               sendclients(f'{addr}: {msg}')
                print(f'{addr}: {msg}')
            
 
      except:
          connected= False
-         sendclient(f"{addr} has connection issues or left the chat")
+
+         sendclients(f"{addr} has connection issues or left the chat")
          print(f"{addr} has connection issues or left the chat")
       
 
@@ -61,11 +65,11 @@ def start():
         ask=input(f"{addr} wants to join to accept type 'accept' else type 'n' ")
         if ask=='accept':
            yes="you are accepted"
+           lcon.append(conn)
+           laddr.append(addr)
            thread = threading.Thread(target=handle_client, args=(conn, addr))
            thread.start()
            conn.send(yes.encode(FORMAT))
-           lcon.append(conn)
-           laddr.append(addr)
            print(addr)
 
            print(f'there are {threading.active_count()-1}')
@@ -74,5 +78,5 @@ def start():
             conn.send(no.encode(FORMAT))
 
 
-print("started........",SERVER)
+print("starting........",SERVER)
 start()
